31ea305557b6f097e33e2e5b709dacce016d0147
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-09-16 12:08:27 +0200

    added baseline db with functionality of migrations when db have been created

--- NewsPaperAuthExample/Program.cs
+++ NewsPaperAuthExample/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NewsPaperAuthExample.Repo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//TODO : add the actual connection string
+builder.Services.AddDbContext<RepoContext>(options =>
+{
+    options.UseSqlServer("WHEN WE HAVE A DB!");
+});
+
+//TODO add auto migration when a migration is pending
+using (var context = new RepoContext(builder.Services.BuildServiceProvider().GetService<DbContextOptions<RepoContext>>()))
+{
+    //check if there are any pending migrations
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        //apply any pending migrations
+        context.Database.Migrate();
+    }
+}
+
 
 var app = builder.Build();
 
---
+++ NewsPaperAuthExample/Repo/RepoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsPaperAuthExample.Entities;
+
+namespace NewsPaperAuthExample.Repo
+{
+    public class RepoContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public RepoContext(DbContextOptions<RepoContext> options) : base(options)
+        {
+        }
+    }
+}
